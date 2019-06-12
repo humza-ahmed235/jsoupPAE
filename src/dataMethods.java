61f2936c679b71c54b599513bcddce9f592ac12b
Humza Ahmed <humza.karachi@gmail.com>
2019-06-12 13:18:35 +0500

    Added contact us but does not have cv links

--- src/dataMethods.java
+++ src/dataMethods.java
@@ -22,22 +22,36 @@ public class dataMethods {
         }
     }
 
-public static void getAnnouncementNews() throws IOException {
-    Document doc = Jsoup.connect("http://www.pacadengg.org/index.html").get();
-    System.out.println("Announcement and news"); // Added to provide a combined heading as i cant at the moment separate
-    Elements ec = doc.select("div.col-2 p");
-    for(Element e : ec) {
+    public static void getAnnouncementNews() throws IOException {
+        Document doc = Jsoup.connect("http://www.pacadengg.org/index.html").get();
+        System.out.println("Announcement and news"); // Added to provide a combined heading as i cant at the moment separate
+        Elements ec = doc.select("div.col-2 p");
+        for (Element e : ec) {
 
-        String oneannounnews = e.select("p").text();// Stores each news piece and date piece
+            String oneannounnews = e.select("p").text();// Stores each news piece and date piece
 
-        System.out.println(oneannounnews);
+            System.out.println(oneannounnews);
 
 
+        }
 
 
     }
 
 
-}
+    public static void contactUS() throws IOException {
+        Document doc = Jsoup.connect("http://www.pacadengg.org/contactus.html").get();
+        Elements ec = doc.select("div.col-2.myfonts p"); // . added to make up for spaces
+Element e2 = doc.select("img").get(1); /* important. I only needed one element so used Element not s.
+      I selected the element with img tag. But there are muliple ElementS. So to keep it singular element(no s)
+      I used the get(1) meaning get second img tag. This way I only get one element(no s) and which is the second one.
+        I then used the abs url thing to convert to a proper link*/
+        System.out.println(e2.absUrl("src"));
+        for (Element e : ec) {
+            System.out.println(e.select("p").text()); // does not get cv links
 
+
+        }
+
+    }
 }
